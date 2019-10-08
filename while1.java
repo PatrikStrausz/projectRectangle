56b8ee36bb33ac474d952e1ee6a4774965ddb17b
patrikstrausz <patrik.strausz@akademiasovy.sk>
2019-10-08 11:51:26 +0200

    1

---
+++ while1.java
@@ -0,0 +1,227 @@
+package sk.itsovy.strausz.other;
+import java.util.Scanner;
+import sk.itsovy.strausz.other.test;
+public class while1 {
+
+    public static void main(String[] args) {
+        test tt= new test();
+        tt.paintRectangle();
+
+        tt.quadraticEqual();
+//        int i = 10000;
+//        while(i<=99999){
+//            int a = i%10;
+//            int b = (i/10)%10;
+//            int c = (i/100)%10;
+//            int d = (i/1000)%10;
+//            int e = (i / 10000)%10;
+//            if(a+b+c+d+e ==43){
+//
+//            System.out.print(i+ " ");
+//                i++;
+//        }
+
+//        int i = 99999;
+//        while (i % 613 != 0) {
+//            i--;
+//        }
+//        System.out.println(i);             //najvyssi spolocny delitel 613
+//
+//              //alebo
+//
+//        int m;
+//        m = 100000 - (100000 % 613);
+//        System.out.println(m);
+
+
+//        int a= 244;
+//        int b=312;
+//        int x=1;                                       //  najnizsi spolocny nasobok
+//        while((a*x) % b !=0){
+//            x++;
+//           }
+//            System.out.println(a*x);
+
+
+//            int a= 10;
+//            int b = 12;
+//            a=a+b;
+//            b=a-b;                            // prehodi a s b a naopak
+//            a=a-b;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        int a = 96;
+//        int b = 27;
+//        int c = 0;
+//        if (a < b) {
+//            c = a;
+//            a = b;
+//            b = c;
+//        }
+//        while (a % b != 0) {        //najvacsi spolocny delitel
+//            c = b;
+//            b = a - b;
+//            a = c;
+//            if (a < b) {
+//                c = a;
+//                a = b;
+//                b = c;
+//
+//
+//            }
+//
+//        }
+//
+//        System.out.println(b);
+
+//        int i= 3;
+//        int j=5;
+//        int k = 8;
+//       while(i<6){                      //uloha 1
+//          int m= i+j+k;
+//           System.out.println(m);
+//           j++; i++;
+//           k-=3;
+//       }
+
+
+//        int i = 5;
+//        int k = 7;
+//        do {
+//            int n = k*i;
+//            i++;
+//            System.out.println(n);          // uloha 2
+//            k= k+3;
+//        } while (5*i<k);
+
+
+//        for(int i=2; i<5;i++){
+//            int j=1;
+//            while(j<=i){
+//                System.out.println("*"); // uloha 3
+//                j++;
+//            }
+//
+//        }
+
+
+//         for(int j=1; j<5; j++){
+//             for(int i = 1;i<=j;i++){        // uloha 4
+//                 System.out.print("**");
+//             }
+//             System.out.println(" ");
+//         }
+
+
+//        for(int j=1; j<4;j++){
+//            for(int i=1; i<6;i++){
+//                System.out.print("*");  //uloha 5
+//            }
+//            System.out.println("");
+//
+//        }
+
+
+//        for (int i = 1; i <= 30; i++) {
+//            for (int j = 1; j <=i; j++) {   // uloha 6
+//                System.out.print("*");
+//
+//            }
+//            System.out.println(" ");
+//        }
+
+
+//        for(int i = 20; i>=1; i--){
+//            for (int j=1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+
+
+//        for(int i = 1;i<=10;i++){
+//            for(int j = 1; j<=i;j++) {
+//
+//
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+
+
+//        for (int r = 1; r <= 10; r++) {
+//
+//            for (int m = 1; m<= 10-r; m++) {
+//                System.out.print(" ");
+//            }
+//                for (int j = 1; j <= r; j++) {
+//                    System.out.print("*");
+//                }
+//            System.out.println(" ");
+//            }
+
+//        for (int r = 10; r >= 1; r--) {
+//
+//            for (int m = 1; m<= 10-r; m++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 1; j <= r; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+
+//
+// for (int i = 1; i <= 7; i++) {
+//            for (int j = 6; j >= i; j -- ) {
+//                System.out.print("  ");
+//            }
+//
+//            for (int k = 1; k <=(i*2-1) ; k ++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println("  ");
+//
+//        }
+
+
+//        for(int i = 1; i<=8;i++){
+//            for(int m=1; m<=8;m++){
+//               if(i==1 || i ==8 || m==1 || m==8){
+//                   System.out.print("# ");
+//               } else {
+//                   System.out.print("  ");
+//               }
+//            }
+//            System.out.println();
+//        }
+
+//        int n = 82654139;
+//        int sum = 0;
+//        while (n > 0) {
+//            int a = n % 10;
+//            sum += a;
+//            n = n / 10;
+//        }
+//        System.out.println(sum);
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
